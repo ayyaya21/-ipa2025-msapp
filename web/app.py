8997a122adfb8f2f9b3150a0515f2dfbe634251b
ayyaya21 <66070221@kmitl.ac.th>
2025-08-19 08:34:03 +0000

    Update: Part7

---
+++ web/app.py
@@ -0,0 +1,47 @@
+import os
+
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from pymongo import MongoClient
+from bson import ObjectId
+
+sample = Flask(__name__)
+
+mongo_uri  = os.environ.get("MONGO_URI")
+db_name    = os.environ.get("DB_NAME")
+
+client = MongoClient(mongo_uri)
+mydb = client["ipa2025"]
+collection = mydb["routers"]
+
+@sample.route("/")
+def main():
+    data = collection.find({}, {'password':0})
+    return render_template("index.html", data=data)
+
+@sample.route("/add", methods=["POST"])
+def add_comment():
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if username and password:
+        collection.insert_one({"ip": ip, "username": username, "password": password})
+    return redirect(url_for("main"))
+
+@sample.route("/delete", methods=["POST"])
+def delete_comment():
+    try:
+        idx = request.form.get("idx")
+        myquery = {'_id': ObjectId(idx)}
+        collection.delete_one(myquery)
+        # if 0 <= idx < len(data):
+        #     data.pop(idx)
+    except Exception:
+        pass
+    return redirect(url_for("main"))
+
+if __name__ == "__main__":
+    sample.run(host="0.0.0.0", port=8080)
